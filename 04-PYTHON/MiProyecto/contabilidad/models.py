66075c17eae2e83c7e9e1d338e664293b03201a9
desardi131 <desardi131@gmail.com>
2024-05-16 18:16:22 +0200

    V0.1.8 MiProyecto Creando la tabla contabilidad_cliente en models.py

--- 04-PYTHON/MiProyecto/contabilidad/models.py
+++ 04-PYTHON/MiProyecto/contabilidad/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# Creando la tabla contabilidad_cliente
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=64)
+    apellidos = models.CharField(max_length=64)
+    rfc = models.CharField(max_length=15, unique=True)
+    fecha_nacimiento = models.DateField()
+    activo = models.BooleanField(default=True)
