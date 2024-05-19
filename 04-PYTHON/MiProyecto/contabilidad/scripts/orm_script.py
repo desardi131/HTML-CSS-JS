f8cf8c883ff46891ac54b923625913f8b4d45fdd
desardi131 <desardi131@gmail.com>
2024-05-19 17:52:58 +0200

    V0.2.1 MiProyecto Introduccion de algunos datos en db

--- 04-PYTHON/MiProyecto/contabilidad/scripts/orm_script.py
+++ 04-PYTHON/MiProyecto/contabilidad/scripts/orm_script.py
@@ -1,2 +1,26 @@
+from contabilidad.models import Cliente, Factura
+import datetime
+
 def run():
-    print('Hello from runscript...')
+    pass
+
+
+'''
+
+    pedro = Cliente(id=1)
+    factura = Factura(
+        cliente = pedro, 
+        importe = 5690.12, 
+        pagada = False
+        )
+    factura.save()
+
+Es lo mismo que esto:
+
+    factura = Factura()
+    factura.cliente = pedro
+    factura.importe = 5690.12
+    factura.pagada = False
+    factura.save
+
+'''
