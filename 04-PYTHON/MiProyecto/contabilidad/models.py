89bacecb843f8c53cd7f9ffea088944600cf5f3e
desardi131 <desardi131@gmail.com>
2024-05-19 12:58:20 +0200

    V0.1.9 MiProyecto models.py clase Factura

--- 04-PYTHON/MiProyecto/contabilidad/models.py
+++ 04-PYTHON/MiProyecto/contabilidad/models.py
@@ -9,3 +9,8 @@ class Cliente(models.Model):
     rfc = models.CharField(max_length=15, unique=True)
     fecha_nacimiento = models.DateField()
     activo = models.BooleanField(default=True)
+
+class Factura(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    importe = models.DecimalField(max_digits=8, decimal_places=2)
+    pagada = models.BooleanField(default=False)
