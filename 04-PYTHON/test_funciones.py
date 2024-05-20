1bb6057c27a5e2dfda83d606644b23de2db9376d
desardi131 <desardi131@gmail.com>
2024-05-20 13:12:52 +0200

    V0.2.2 Test_funciones.py con unittest completo

--- 04-PYTHON/test_funciones.py
+++ 04-PYTHON/test_funciones.py
@@ -2,9 +2,31 @@ import unittest
 from funciones import es_primo
 
 class TestEs_primo(unittest.TestCase):
+    # Test de valores conocidos, debemos introducir valores que sabemos que la solucion es correcta
     def test_es_primo(self):
         print('-----Test valores de resultado conocido')
-        self.assertRaises(TypeError,es_primo,13.4)
-        self.assertRaises(TypeError,es_primo,'hola')
-        self.assertRaises(TypeError,es_primo,2+3j)
+
+        self.assertAlmostEqual(es_primo(5),True)
+        self.assertAlmostEqual(es_primo(6),False)
+        self.assertAlmostEqual(es_primo(31),True)
+        self.assertAlmostEqual(es_primo(15),False)
+
+    # Test de valores negativos
+    def test_negativos(self):
+        print('-----Test de valores negativos----')
+    #Indicamos el tipo de excepción, la función y el valor esperado.
+
+        self.assertRaises(ValueError,es_primo,-12)
+        self.assertRaises(ValueError,es_primo,0)
+        self.assertRaises(ValueError,es_primo,-31)
+    
+    # Test de tipos no compatibles. Verificamos si el tipo de los parámetros es el correcto
+    # El tipo de la excepcion debe ser TypeError
+    # Hacemos una prueba para cada tipo conocido no válido
+    def test_tipos(self):
+        print('-----Test de tipos no compatibles----')
+
         self.assertRaises(TypeError,es_primo,True)
+        self.assertRaises(TypeError,es_primo,'Hola')
+        self.assertRaises(TypeError,es_primo,2+3j)
+        self.assertRaises(TypeError,es_primo,12.5)
